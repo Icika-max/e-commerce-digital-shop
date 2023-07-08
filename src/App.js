496222b9006a18839107f0ab35980396eb18fa8e
icika-max <icikakennedy@gmail.com>
2023-07-08 09:35:59 +0300

    fetchig of the products

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { commerce } from './lib/commerce'
 import Products from './component/products/Products'
 import Navbar from './component/Navbar/Navbar'
 // import {Products,Navbar} from './component'
--- src/component/products/Products.jsx
+++ src/component/products/Products.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
-
+import useStyles from './styles'
 const products=[
     {id:1, name:'macbook', description: 'macbook air', price:'$30', image: 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1926&q=80'},
     {id:2, name:'Hp', description: 'Hp Envy Ryzen', price:'$20', image:'https://images.unsplash.com/photo-1583223667854-e0e05b1ad4f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'}
@@ -9,8 +9,10 @@ const products=[
 ]
 
 const Products = () => {
+    const classes= useStyles()
   return (
-<main>
+<main className={classes.content}>
+    <div className={classes.toolbar}/>
     <Grid container justifyContent='center' spacing={4}>
         {
             products.map((product)=>(
